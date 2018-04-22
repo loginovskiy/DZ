462f36f20e7ca0d49385413692a3d0f95135e216
loginovskiy <vv.loginovskiy@gmail.com>
2018-04-22 23:28:46 +0300

    Добавлен массив и разграниченны цыклы

--- src/ru/vyacheslav/lessons/WordGame.java
+++ src/ru/vyacheslav/lessons/WordGame.java
@@ -6,8 +6,37 @@ public class WordGame
     public static void main(String[] args)
     {
         int menu, rndNum;
-        String rndWord, usrWord;
-        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avacado", "broccol}
+        String rndWord;
+        String usrWord;
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avacado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "qiwi", "mango", "mashroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
+        Scanner scan = new Scanner(System.in);
+        Random rnd = new Random();
 
+        System.out.println("Начать -1, выйти -0");
+        do
+        {
+            menu = scan.nextInt();
+        }while(menu>1 || menu<0);
+        if(menu==1)
+        {
+            do
+            {
+                rndNum = rnd.nextInt(words.length);
+                rndWord = words[rndNum];
+                System.out.println(rndWord);
+                System.out.println("Было загаданно случайное слово из предлженных");
+                System.out.print("Введите слово: ");
+                scan.nextLine();
+                usrWord = scan.nextLine();
+                do
+                {
+                    menu=scan.nextInt();
+                }while(menu<0 | menu >1);
+            }while(menu==1);
+        }
+        else;
+        scan.close();
     }
 }
