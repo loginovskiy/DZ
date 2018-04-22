d4f619eba1d8fcb262d7838a7df17355d988e3df
loginovskiy <vv.loginovskiy@gmail.com>
2018-04-22 12:28:17 +0300

    Добавил Объект Scanner и объект Random, а также 3-и целочисленные переменные.

---
+++ src/ru/vyacheslav/lessons/NumbersGame.java
@@ -0,0 +1,12 @@
+package ru.vyacheslav.lessons;
+import java.util.Random;
+import java.util.Scanner;
+public class NumbersGame
+{
+    public static void main(String[] args)
+    {
+        int rndNum, usrNum, menu;
+        Random rnd = new Random();
+        Scanner scan = new Scanner(System.in);
+    }
+}
