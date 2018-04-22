d83953fc24687b347334a6e7226985677535cacd
loginovskiy <vv.loginovskiy@gmail.com>
2018-04-22 20:01:17 +0300

    Добавлен класс WordGame

---
+++ src/ru/vyacheslav/lessons/WordGame.java
@@ -0,0 +1,13 @@
+package ru.vyacheslav.lessons;
+import java.util.Random;
+import java.util.Scanner;
+public class WordGame
+{
+    public static void main(String[] args)
+    {
+        int menu, rndNum;
+        String rndWord, usrWord;
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avacado", "broccol}
+
+    }
+}
