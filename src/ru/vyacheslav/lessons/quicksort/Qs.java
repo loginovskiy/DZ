47872929565ad4c7d210f07146d7e06c854ef59a
loginovskiy <vv.loginovskiy@gmail.com>
2018-05-06 08:22:04 +0300

    Пример класса быстрой сортировки

---
+++ src/ru/vyacheslav/lessons/quicksort/Qs.java
@@ -0,0 +1,14 @@
+package ru.vyacheslav.lessons.quicksort;
+
+public class Qs
+{
+    int[] array;
+    int left, right;
+    public Qs(int[] arr, int left, int right)
+    {
+        this.array=arr;
+        this.left=left;
+        this.right=right;
+    }
+
+}
---
+++ src/ru/vyacheslav/lessons/quicksort/QuickSort.java
@@ -0,0 +1,16 @@
+package ru.vyacheslav.lessons.quicksort;
+import java.util.Arrays;
+import java.util.Random;
+public class QuickSort
+{
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+        int[] array = new int[20];
+        for (int i = 0; i <array.length ; i++)
+        {
+            array[i]=rnd.nextInt(50);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
